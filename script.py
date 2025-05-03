c94a848f44b835c37b63169388b5c6fc06d17487
Harshiljethva73 <harshil.jethva1@gmail.com>
2025-05-03 17:13:26 +0530

    Fix: remove hardcoded port for Render

--- script.py
+++ script.py
@@ -123,5 +123,8 @@ from flask import send_file
 def home():
     return send_file('index.html')
 
+import os
+
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
